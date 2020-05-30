0afbe8b4853afaf03f66d8e37ceac12ceb40393c
Matheus Alcantara <matheusssilv97@gmail.com>
2020-05-29 21:01:03 -0300

    change exercise 1 of chapter 8 to use pattern match and tests

--- ch8/exercise1/src/main.rs
+++ ch8/exercise1/src/main.rs
@@ -1,12 +1,16 @@
 use std::collections::HashMap;
 
 fn main() {
-    let values = vec![10, 20, 30, 10, 40];
-    let map = compute(values);
-    println!("{:?}", map);
+    let values = vec![10, 10, 20, 5];
+    let map_option = compute(values);
+
+    match map_option {
+        None => println!("Invalid values"),
+        Some(map) => println!("{:?}", map),
+    }
 }
 
-fn compute(values: Vec<usize>) -> HashMap<String, usize> {
+fn compute(values: Vec<usize>) -> Option<HashMap<String, usize>> {
     let mut count_values = HashMap::new();
     let mut sum_values = 0;
 
@@ -16,27 +20,61 @@ fn compute(values: Vec<usize>) -> HashMap<String, usize> {
         sum_values += value;
     }
 
-    let mean = sum_values / values.len();
+    let values_len = values.len();
+
+    if values_len == 0 {
+        return None;
+    }
+
+    let mean = sum_values / values_len;
 
     let mut values_sorted = values.clone();
     values_sorted.sort();
     let middle_value = values_sorted.len() / 2;
 
-    let max_count_value = *count_values.values().max().unwrap();
-    let mode = *count_values
-        .iter()
-        .find_map(|(key, &val)| {
-            if val == max_count_value {
-                Some(*key)
-            } else {
-                None
-            }
-        })
-        .unwrap();
+    match count_values.values().max() {
+        None => None,
+        Some(value) => {
+            let mode_option =
+                count_values.iter().find_map(
+                    |(key, &val)| {
+                        if val == *value {
+                            Some(*key)
+                        } else {
+                            None
+                        }
+                    },
+                );
 
-    let mut map = HashMap::new();
-    map.insert(String::from("mean"), mean);
-    map.insert(String::from("median"), values_sorted[middle_value]);
-    map.insert(String::from("mode"), mode);
-    map
+            match mode_option {
+                None => None,
+                Some(mode) => {
+                    let mut map = HashMap::new();
+                    map.insert(String::from("mean"), mean);
+                    map.insert(String::from("median"), values_sorted[middle_value]);
+                    map.insert(String::from("mode"), *mode);
+
+                    Some(map)
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn compute_return_some() {
+        let values = vec![5, 5, 10, 20, 5];
+
+        let mut values_expected = HashMap::new();
+        values_expected.insert(String::from("mean"), 9);
+        values_expected.insert(String::from("median"), 5);
+        values_expected.insert(String::from("mode"), 5);
+
+        let values_got = compute(values).unwrap();
+
+        assert_eq!(values_got, values_expected);
+    }
 }
